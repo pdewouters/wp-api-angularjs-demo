37c6439895bf1a5df6df057f93690a984fac706c
Paul de Wouters <paul@hmn.md>
2014-08-03 09:52:48 -0300

    Update script loading to use Grunt generated minified app js

--- functions.php
+++ functions.php
@@ -15,26 +15,7 @@ add_action( 'wp_enqueue_scripts', function(){
 
 	wp_enqueue_style( 'foundation', 'http://cdn.foundation5.zurb.com/foundation.css' );
 
-	wp_enqueue_style( 'ng-tags-input', get_template_directory_uri() . '/app/bower_components/ng-tags-input/ng-tags-input.min.css' );
-
-	wp_register_script( 'wpc-angular', get_template_directory_uri() . '/app/bower_components/angular/angular.min.js' );
-
-	wp_register_script( 'wpc-angular-resource', get_template_directory_uri() . '/app/bower_components/angular-resource/angular-resource.min.js' );
-
-	wp_register_script( 'wpc-angular-router', get_template_directory_uri() . '/app/bower_components/angular-ui-router/release/angular-ui-router.min.js' );
-
-	wp_enqueue_script( 'checklist-model', get_template_directory_uri() . '/app/checklist-model.js', array( 'wpc-angular' ), THEME_VERSION );
-
-	wp_enqueue_script( 'ng-tags-input', get_template_directory_uri() . '/app/bower_components/angular-sanitize/angular-sanitize.min.js', array( 'wpc-angular' ), THEME_VERSION );
-
-	wp_enqueue_script( 'ng-sanitize', get_template_directory_uri() . '/app/bower_components/ng-tags-input/ng-tags-input.js', array( 'wpc-angular' ), THEME_VERSION );
-
-	wp_register_script( 'wpc-app',
-		get_template_directory_uri() . '/app/app.js',
-		array( 'wpc-angular', 'wpc-angular-resource', 'wpc-angular-router', 'checklist-model', 'angular-wp-api' ), THEME_VERSION );
-
-
-	wp_localize_script( 'wpc-app', 'wpc_app', array( 'templateUrl' => THEME_PARTIALS_URL ) );
+	wp_register_script( 'wpc-app', get_template_directory_uri() . '/includes/js/app.min.js', array( 'angular-wp-api' ), THEME_VERSION );
 
 	wp_enqueue_script( 'wpc-app' );
 
