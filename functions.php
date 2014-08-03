40d51b08b5d2ab993b95dba8837378b305d58755
Paul de Wouters <paul@hmn.md>
2014-08-03 17:48:56 -0300

    Hook into WP API to register CPT
    Signed-off-by: Paul de Wouters <paul@hmn.md>

--- functions.php
+++ functions.php
@@ -18,6 +18,13 @@ add_action( 'wp_enqueue_scripts', function(){
 	wp_register_script( 'wpc-app', get_template_directory_uri() . '/includes/js/app.min.js', [ 'angular-wp-api' ], THEME_VERSION );
 
 	wp_enqueue_script( 'wpc-app' );
+add_action( 'wp_json_server_before_serve', function( $server ) {
+
+		global $wpc_stars;
+
+		require_once get_template_directory() . '/includes/stars/class-stars-wp-api.php';
+		$wpc_stars = new WP_JSON_Stars( $server );
+		$wpc_stars->register_filters();
 
 });
 
