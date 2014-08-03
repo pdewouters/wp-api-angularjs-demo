66d4bf9a3e596f00d6592a8b49d70c7f4f095d03
Paul de Wouters <paul@hmn.md>
2014-08-03 10:07:53 -0300

    Fix dependencies and loading

--- functions.php
+++ functions.php
@@ -8,14 +8,14 @@ if ( ! defined( 'THEME_VERSION' ) ) {
 }
 
 add_action( 'after_setup_theme', function() {
-	add_theme_support( 'angular-wp-api', array( 'wpc-angular', 'wpc-angular-resource' ) );
+	add_theme_support( 'angular-wp-api' );
 } );
 
 add_action( 'wp_enqueue_scripts', function(){
 
 	wp_enqueue_style( 'foundation', 'http://cdn.foundation5.zurb.com/foundation.css' );
 
-	wp_register_script( 'wpc-app', get_template_directory_uri() . '/includes/js/app.min.js', array( 'angular-wp-api' ), THEME_VERSION );
+	wp_register_script( 'wpc-app', get_template_directory_uri() . '/includes/js/app.min.js', [ 'angular-wp-api' ], THEME_VERSION );
 
 	wp_enqueue_script( 'wpc-app' );
 
